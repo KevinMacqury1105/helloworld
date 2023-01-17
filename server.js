bb65e62c3cff9a983ff23e9d06b95b3f811935f4
kevin <kevin@demo.com>
2023-01-17 18:13:23 -0500

    initial commit

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var app = express();
 
 // setup a 'route' to listen on the default url path
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+    res.send("Kevin Macqury - 140327206");
 });
 
 // setup http server to listen on HTTP_PORT
