1ba3e704177ec7b723e9102a2c6a444aef4e84d5
kevin <kevin@demo.com>
2023-01-18 06:06:17 -0500

    initial change

--- server.js
+++ server.js
@@ -1,3 +1,22 @@
+/*********************************************************************************
+*  WEB322 – Assignment 1
+*  I declare that this assignment is my own work in accordance with Seneca Academic Policy.  
+*  No part of this assignment has been copied manually or electronically from any other source
+*  (including web sites) or distributed to other students.
+* 
+*  Name: ___Kevin Macqury Sivarajakumaran___________________ Student ID: __140327206____________ Date: _____01/18/2023___________
+*
+*  Cyclic Web App URL: ___
+_https://nice-teal-beaver-boot.cyclic.app_________________________________________________
+*
+*  GitHub Repository URL: ___
+Your Repositories (github.com)__
+
+KevinMacqury1105/helloworld(github.com)________________________________________________
+*
+********************************************************************************/ 
+
+
 var HTTP_PORT = process.env.PORT || 8080;
 var express = require("express");
 var app = express();
